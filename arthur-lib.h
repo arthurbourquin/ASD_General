88a4b53cd0e7cc962a50c1d3505ef202f51d3fda
arthur bourquin <arthur@tohm.ch>
2025-02-19 21:28:39 +0100

    update

--- arthur-lib.h
+++ arthur-lib.h
@@ -1,31 +1,74 @@
-#ifndef _TRI_A_BULLE_
-#define _TRI_A_BULLE_
+#ifndef _ARTHUR_LIB_
+#define _ARTHUR_LIB_
 
 #include <iostream>
 #include <vector>
 #include <string>
 using namespace std;
 
+
+template<typename It>
+void print_vector(It first, It last, string separator = "\t") {
+    if(first == last) return;
+    for(; first != prev(last); ++first) {
+        cout << *first << separator;
+    }
+    cout << *prev(last) << endl;
+}
+
 template<typename T>
 void print_vector(const vector<T> & v, string separator = "\t") {
-    if(v.empty()) return;
-    for(size_t i = 0; i < v.size() - 1; ++i) {
-        cout << v.at(i) << separator;
-    }
-    cout << v.back() << endl;
+    print_vector(v.begin(), v.end(), separator);
 }
 
 template<typename It>
-void tri_a_bulle(It first, It last) {
+bool bubble_sort(It first, It last) { 
+    if(first == last) return false;
     bool swapped = false;
     It current = first;
-        while (current != prev(last)) {
-            if (*current > *next(current)) {
+    while(last != first) {
+        while(current != prev(last)) {
+            if(*current > *next(current)) {
                 swap(*current, *next(current));
                 swapped = true;
             }
             ++current;
         }
+        if(swapped == false) break;
+        current = first;
+        --last;
+    }
+    return swapped;
+}
+
+template<typename T>
+bool bubble_sort(vector<T> & v) {
+    return tri_a_bulle(v.begin(), v.end());
+}
+
+template<typename It>
+bool selection_sort(It first, It last) {
+    if(first == last) return false;
+    bool swapped = false;
+    for(; first != prev(last); ++first) {
+        It current = next(first);
+        It min = current;
+        for(; current != last; ++current) {
+            if(*current < *min) {
+                min = current;
+            }
+        }
+        if(*min < *first) {
+            swap(*min, *first);
+            swapped = true;
+        }
+    }
+    return swapped;
+}
+
+template<typename T>
+bool selection_sort(vector<T> & v) {
+    return selection_sort(v.begin(), v.end());
 }
 
 
--- main.cpp
+++ main.cpp
@@ -1,16 +1,14 @@
 #include "arthur-lib.h"
 
-vector<int> v = {4, 2, 5, 1, 3};
+vector<int> v = {4, 2, 7, 9, 5, 1, 3, 6};
 
 int main() {
 
-    /*
-    print_vector(v, ", ");
-    tri_a_bulle(v.begin(), v.end());
-    print_vector(v, ", ");
-    */
 
-    cout << v.back() << endl;
+    print_vector(v, ", ");
+    selection_sort(v.begin(), v.end());
+    print_vector(v, ", ");
+
 
     return 0;
 }
